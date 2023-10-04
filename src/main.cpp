15e660bc3d433d636cdafe16a0759170653456e9
Hugo <frug.batt.dev@gmail.com>
2023-10-04 17:17:21 +0200

    adding play sound

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,66 @@
+// Forcing OS to linux (useful for Stk)
+#define __OS_LINUX__
+
 #include <gtkmm.h>
 
+#include <iostream>
+
+#include "SineWave.h"
+#include "RtWvOut.h"
+
+using namespace stk;
+using namespace Gtk;
+
+// Gtk components
+Window* window;
+Box* box;
+Button* button;
+Scale* scale;
+
+RtWvOut* dac;
+
+
+// Function prototypes
+void play_sound();
+
+
+
+// Main function
 int main(int argc, char *argv[])
 {
-  auto app = Gtk::Application::create(argc, argv, "synthetisens.app");
+  auto app = Application::create(argc, argv, "synthetisens.app");
 
-  Gtk::Window window;
-  window.set_default_size(200, 200);
-  window.set_title("synthetisENS");
+  window = new Window();
+  window->set_default_size(200, 200);
+  window->set_title("synthetisENS");
 
-  return app->run(window);
+  box = new Box(ORIENTATION_VERTICAL);
+
+  button = new Button("Play sound");
+  button->signal_clicked().connect(sigc::ptr_fun(&play_sound));
+  box->add(*button);
+
+  scale = new Scale(ORIENTATION_HORIZONTAL);
+  scale->set_range(0.0, 1000.0);
+  box->add(*scale);
+
+  window->add(*box);
+  window->show_all_children();
+  
+
+  Stk::setSampleRate(44100.0);
+  Stk::showWarnings(true);
+
+  return app->run(*window);
+}
+
+
+// Function definitions
+void play_sound()
+{
+  dac = new RtWvOut( 1 );
+  SineWave sine;
+  sine.setFrequency(scale->get_value());
+  for (int i = 0; i < 70000; i++) dac->tick(sine.tick());
+  delete dac;
 }
