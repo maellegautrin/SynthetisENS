f399233656b97577c358677237215f4d9e31d259
Slukoo <simon.lukowski@hotmail.com>
2023-10-11 09:17:38 +0200

    wave shapes and duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 // Forcing OS to linux (useful for Stk)
 #define __OS_LINUX__
 
+#define sine_id 1
+#define square_id 2
+#define sawtooth_id 3
+
+
+
 #include <gtkmm.h>
 
 #include <iostream>
 
+#include <BlitSaw.h>
+#include <BlitSquare.h>
 #include "SineWave.h"
 #include "RtWvOut.h"
 
@@ -15,13 +23,22 @@ using namespace Gtk;
 Window* window;
 Box* box;
 Button* button;
-Scale* scale;
+Button* sinebutton;
+Button* squarebutton;
+Button* sawbutton;
+Scale* freq;
+Scale* duration;
+
+int wave = sine_id;
 
 RtWvOut* dac;
 
 
 // Function prototypes
 void play_sound();
+void set_sine();
+void set_square();
+void set_saw();
 
 
 
@@ -29,6 +46,7 @@ void play_sound();
 int main(int argc, char *argv[])
 {
   auto app = Application::create(argc, argv, "synthetisens.app");
+  wave = sine_id;
 
   window = new Window();
   window->set_default_size(200, 200);
@@ -40,9 +58,25 @@ int main(int argc, char *argv[])
   button->signal_clicked().connect(sigc::ptr_fun(&play_sound));
   box->add(*button);
 
-  scale = new Scale(ORIENTATION_HORIZONTAL);
-  scale->set_range(0.0, 1000.0);
-  box->add(*scale);
+  sinebutton = new Button("Sine wave");
+  sinebutton->signal_clicked().connect(sigc::ptr_fun(&set_sine));
+  box->add(*sinebutton);
+
+  squarebutton = new Button("Square wave");
+  squarebutton->signal_clicked().connect(sigc::ptr_fun(&set_square));
+  box->add(*squarebutton);
+
+  sawbutton = new Button("Sawtooth wave");
+  sawbutton->signal_clicked().connect(sigc::ptr_fun(&set_saw));
+  box->add(*sawbutton);
+
+  freq = new Scale(ORIENTATION_HORIZONTAL);
+  freq->set_range(0.0, 1000.0);
+  box->add(*freq);
+
+  duration = new Scale(ORIENTATION_HORIZONTAL);
+  duration->set_range(0.0, 100000.0);
+  box->add(*duration);
 
   window->add(*box);
   window->show_all_children();
@@ -55,12 +89,38 @@ int main(int argc, char *argv[])
 }
 
 
+void set_sine(){
+  wave = sine_id;
+}
+
+
+void set_square(){
+  wave = square_id;
+}
+
+
+void set_saw(){
+  wave = sawtooth_id;
+}
+
 // Function definitions
 void play_sound()
 {
   dac = new RtWvOut( 1 );
+  BlitSquare square;
+  BlitSaw sawtooth;
   SineWave sine;
-  sine.setFrequency(scale->get_value());
-  for (int i = 0; i < 70000; i++) dac->tick(sine.tick());
+  if(wave == sine_id){
+    sine.setFrequency(freq->get_value());
+    for (int i = 0; i < duration->get_value(); i++) dac->tick(sine.tick());
+  }
+  if(wave == square_id){
+    square.setFrequency(freq->get_value());
+    for (int i = 0; i < duration->get_value(); i++) dac->tick(square.tick());
+  }
+  if(wave == sawtooth_id){
+    sawtooth.setFrequency(freq->get_value());
+    for (int i = 0; i < duration->get_value(); i++) dac->tick(sawtooth.tick());
+  }
   delete dac;
 }
